05403e1eca38e5f27c6ddcbb0e6829c166be288f
wizzy wilson <varghesewilson61@gmail.com>
2019-04-22 10:06:03 +0530

    added tollbar and home with minimal routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
 import { Angular2TokenService } from 'angular2-token';
 import { HttpModule } from '@angular/http';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Angular2TokenService} from "angular2-token";
-import {environment} from "../../environments/environment";
+import { Angular2TokenService } from "angular2-token";
+import { environment } from "../../environments/environment";
 
 @Component({
   selector: 'app-register',
@@ -9,21 +9,8 @@ import {environment} from "../../environments/environment";
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private authToken: Angular2TokenService){
-    this.authToken.init(environment.token_auth_config);
-    this.authToken.signIn({email: "user@example.com", password: "monkey67"}).subscribe(
-
-      res => {
-
-        console.log('auth response:', res);
-        console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
-        console.log('auth response body:', res.json()); //log the response body to show the user 
-      },
-
-      err => {
-        console.error('auth error:', err);
-      }
-  )
+  constructor(private tokenAuthService: Angular2TokenService){
+    this.tokenAuthService.init(environment.token_auth_config);
   }
 
   ngOnInit() {
